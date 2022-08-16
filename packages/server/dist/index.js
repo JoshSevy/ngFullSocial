356c05fe53aaf63f70a1a9b6205aaf9e96746399
JoshSevy <joshuasevy@outlook.com>
2022-08-15 22:41:19 -0600

    Start building server

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
 const PORT = 8080;
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
@@ -12,3 +22,33 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+const typeDefs = (0, apollo_server_express_1.gql) `
+  type Query {
+    message: String!
+  }
+`;
+const reslovers = {
+    Query: {
+        message: () => 'It works!',
+    },
+};
+const config = {
+    typeDefs: typeDefs,
+    resolvers: reslovers,
+};
+function startApolloServer(config) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const PORT = 8080;
+        const app = (0, express_1.default)();
+        const server = new apollo_server_express_1.ApolloServer(config);
+        yield server.start();
+        server.applyMiddleware({
+            app,
+            path: '/graphql',
+        });
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    });
+}
+startApolloServer(config);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,6 @@
+import { IResolvers } from '@graphql-tools/utils';
 import express, { Application } from 'express';
+import { ApolloServer, Config, gql } from 'apollo-server-express';
 
 const PORT = 8080;
 const app: Application = express();
@@ -9,3 +11,37 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const typeDefs: any = gql`
+  type Query {
+    message: String!
+  }
+`;
+
+const reslovers: IResolvers = {
+  Query: {
+    message: () => 'It works!',
+  },
+};
+
+const config: Config = {
+  typeDefs: typeDefs,
+  resolvers: reslovers,
+};
+
+async function startApolloServer(config: Config) {
+  const PORT = 8080;
+  const app: Application = express();
+  const server: ApolloServer = new ApolloServer(config);
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql',
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startApolloServer(config);
